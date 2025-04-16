a7a8bc7da81ab2468c1735fb33466368930f1758
Brian Jorgensen <brian.jorgensen@gmail.com>
2025-04-15 19:32:21 -0600

    get rid of exit message

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #[tokio::main]
 async fn main() {
     match komrad_cli::cli::main().await {
-        Ok(_) => println!("REPL exited successfully."),
+        Ok(_) => {}
         Err(e) => eprintln!("Error: {}", e),
     }
 }
